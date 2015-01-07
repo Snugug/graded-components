6c27035c11a81f188765ffb74659ce6e07eb6b96
Sam Richard <snugug@gmail.com>
2015-01-07 12:51:21 -0500

    Enhanced component based on network latency

--- js/listing.js
+++ js/listing.js
@@ -2,28 +2,67 @@
   'use strict';
 
   window.addEventListener('DOMContentLoaded', function () {
-    var mapLinks = document.querySelectorAll('.listing--address'),
+    var listings = document.querySelectorAll('.listing'),
+        latency = window.performance.timing.responseStart - window.performance.timing.requestStart,
+        preview,
+        img,
         wrapper,
         map,
         listing,
+        address,
         i;
 
-    for (i = 0; i < mapLinks.length; i++) {
-      parent = mapLinks[i].parentNode.parentNode;
-      listing = mapLinks[i].getAttribute('data-address');
+    console.log(latency);
 
-      wrapper = document.createElement('div');
-      map = document.createElement('iframe');
-      wrapper.setAttribute('class', 'listing--map');
-      map.setAttribute('frameborder', 0);
+    //////////////////////////////
+    // Only enhance if on a low latency connection
+    //////////////////////////////
+    if (latency < 200) {
+      // Load images on DOMContentLoaded
+      for (i = 0; i < listings.length; i++) {
+        listing = listings[i];
+        listing.setAttribute('data-image', true);
 
-      map.setAttribute('src', 'https://www.google.com/maps/embed/v1/place?q=' + listing + '&key=AIzaSyD6ATXLbrPCgBoeLzcWD1AphoYxKxwbNjE');
+        preview = listing.querySelector('.listing--preview');
 
-      wrapper.appendChild(map);
+        if (preview) {
+          img = document.createElement('img');
+          img.setAttribute('src', preview.getAttribute('data-src'));
+          img.setAttribute('alt', preview.getAttribute('data-alt'));
+          img.setAttribute('class', 'listing--image');
 
-      parent.appendChild(wrapper);
+          preview.appendChild(img);
+        }
 
-      parent.children[0].children[2].style.display = 'none';
+        // Hide listing view button here as we don't want it to flash on load
+
+        if (latency < 100) {
+          listing.querySelector('.listing--view').style.display = 'none';
+        }
+
+      }
+
+      // Load Google Maps link on load
+      window.addEventListener('load', function () {
+        if (latency < 100) {
+          for (i = 0; i < listings.length; i++) {
+            listing = listings[i];
+            address = encodeURIComponent(listing.querySelector('.listing--address').innerText);
+
+            wrapper = document.createElement('div');
+            map = document.createElement('iframe');
+
+            wrapper.setAttribute('class', 'listing--map');
+            map.setAttribute('frameborder', 0);
+
+            map.setAttribute('src', 'https://www.google.com/maps/embed/v1/place?q=' + address + '&key=AIzaSyD6ATXLbrPCgBoeLzcWD1AphoYxKxwbNjE');
+
+            wrapper.appendChild(map);
+
+            listing.appendChild(wrapper);
+          }
+        }
+      });
     }
   });
 }());
